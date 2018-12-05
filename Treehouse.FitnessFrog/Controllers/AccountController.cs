c5bc182d787395b562e8880e5c268f3cae199b55
Anton Miroshkin <anton.miroshkin@gmail.com>
2018-12-05 13:55:25 +0300

    Added account controller

---
+++ Treehouse.FitnessFrog/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+using Treehouse.FitnessFrog.ViewModels;
+
+namespace Treehouse.FitnessFrog.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(AccountRegisterViewModel viewModel)
+        {
+            // If the ModelState is valid...
+
+            // Instantiate a User object
+
+            // Create the user
+
+            // If the user was successfully created...
+
+            // Sign-in the user and redirect them to the web app's "Home" page
+
+            // If there were errors...
+
+            // Add model errors
+
+            return View(viewModel);
+        }
+    }
+}
