1c88a5dd7f7a16b63f0f3fdf731737a1133cd6c1
Anton Miroshkin <anton.miroshkin@gmail.com>
2018-12-11 09:38:21 +0300

    Added async register method

--- Treehouse.FitnessFrog/Controllers/AccountController.cs
+++ Treehouse.FitnessFrog/Controllers/AccountController.cs
@@ -1,18 +1,32 @@
-﻿using System.Web.Mvc;
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.Owin.Security;
 using Treehouse.FitnessFrog.Shared.Models;
+using Treehouse.FitnessFrog.Shared.Security;
 using Treehouse.FitnessFrog.ViewModels;
 
 namespace Treehouse.FitnessFrog.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly ApplicationUserManager _userManager;
+        private readonly ApplicationSignInManager _signInManager;
+        private readonly IAuthenticationManager _authenticationManager;
+
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager, IAuthenticationManager authenticationManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _authenticationManager = authenticationManager;
+        }
+
         public ActionResult Register()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult Register(AccountRegisterViewModel viewModel)
+        public async Task<ActionResult> Register(AccountRegisterViewModel viewModel)
         {
             // If the ModelState is valid...
             if (ModelState.IsValid)
@@ -21,14 +35,24 @@ namespace Treehouse.FitnessFrog.Controllers
                 var user = new User { UserName = viewModel.Email, Email = viewModel.Email };
 
                 // Create the user
+                var result = await _userManager.CreateAsync(user, viewModel.Password);
 
                 // If the user was successfully created...
+                if (result.Succeeded)
+                {
+                    // Sign-in the user and redirect them to the web app's "Home" page
+                    await _signInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+                    return RedirectToAction("Index", "Entries");
 
-                // Sign-in the user and redirect them to the web app's "Home" page
+                }
 
                 // If there were errors...
-
                 // Add model errors
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error);
+                }
             }
 
 
